cbac9ccb042d72e7ce1ea5b554f284141537f266
T31K <t31kmunwong@gmail.com>
2020-10-07 13:28:41 +0800

    Added about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
+import {Container, Row, Col} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {fetchapi} from "./fetchapi"
+import { faLaptop, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const customCard = {
+  background: "#f6def6",
+  height: "95vh",
+  margin : "0"
+}
+
+const fontColor = {
+  color: "#373a40"
+}
 
 
 
@@ -16,12 +29,13 @@ class About extends Component{
   render(){
 
     return (
-    
-      <div>
-      <h1>About Page</h1>
-   
+      <Container>
+      <div className="card mb-4" style={customCard}>
+      
+      <h1 className="mt-4 text-center" style={fontColor}>ABOUT  <FontAwesomeIcon icon={faLaptopCode} /></h1>
+      
 </div>
-    
+</Container>
   )}
 }
 
