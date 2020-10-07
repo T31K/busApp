4c80b6453fcd0d999cd18d4cb28643e2584e3189
T31K <t31kmunwong@gmail.com>
2020-10-07 13:43:57 +0800

    Update About page

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import {Container, Row, Col} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {fetchapi} from "./fetchapi"
-import { faLaptop, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faLaptop, faLaptopCode, faCheckSquare, faQuestionCircle, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const customCard = {
@@ -33,8 +33,18 @@ class About extends Component{
       <div className="card mb-4" style={customCard}>
       
       <h1 className="mt-4 text-center" style={fontColor}>ABOUT  <FontAwesomeIcon icon={faLaptopCode} /></h1>
+      <div className="text-left mx-auto">
+        <p className="mt-4 "><FontAwesomeIcon icon={faQuestionCircle} /> HAVE an extra tablet lying around in your house</p>
+        <p className=""><FontAwesomeIcon icon={faQuestionCircle} /> HATE expensive ride hailing services fee</p>
+        <p className=""><FontAwesomeIcon icon={faQuestionCircle} /> LOVE to take the public transport but hate to wait</p>
+      </div>
+
+      <div className="text-left mx-auto mt-4">
+      <h3 className=""><FontAwesomeIcon icon={faThumbsUp} /> This is the app for you then</h3>
+ 
+      </div>
       
-</div>
+      </div>
 </Container>
   )}
 }
